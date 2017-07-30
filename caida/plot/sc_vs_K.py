26a2b1458020cf6c944ed4bddc5404f56653a5db
cchliu <cchliu@ucdavis.edu>
2017-07-30 12:31:08 -0700

    final results plotting figures

---
+++ caida/plot/sc_vs_K.py
@@ -0,0 +1,63 @@
+
+
+leaf_level = [8, 9, 10, 11, 12]
+sc = [469, 1271, 469, 569, 715]  
+p_rwcb = [100.0, 100.0, 100.0, 100, 100.0]
+r_rwcb = [100.0, 99.5, 99.9, 100, 100.0]
+
+p_minlan = [81.5595238095, 80.0701162355, 66.498447205, 63.3397108844, 59.2689470154]
+r_minlan = [70.9420289855, 60.7424071991, 51.2422360248, 43.3673469388, 47.9879275654]
+
+
+
+"""
+rwcb:
+leaf_level = 8
+Monte carlo iterations:  200
+Avg time interval:  468.84
+Avg precision:  100.0
+Avg recall:  100.0
+
+Monte carlo iterations:  200
+Avg time interval:  1270.325
+Avg precision:  100.0
+Avg recall:  99.5
+
+Monte carlo iterations:  200
+Avg time interval:  468.965
+Avg precision:  100.0
+Avg recall:  99.9285714286
+
+Monte carlo iterations:  200
+Avg time interval:  568.38
+Avg precision:  100.0
+Avg recall:  100.0
+
+Monte carlo iterations:  200
+Avg time interval:  714.55
+Avg precision:  100.0
+Avg recall:  100.0
+
+
+minlan:
+Monte carlo runs: 10
+Avg precision:  81.5595238095
+Avg recall:  70.9420289855
+
+Monte carlo runs: 10
+Avg precision:  80.0701162355
+Avg recall:  60.7424071991
+
+Monte carlo runs: 10
+Avg precision:  66.498447205
+Avg recall:  51.2422360248
+
+Monte carlo runs: 10
+Avg precision:  63.3397108844
+Avg recall:  43.3673469388
+
+Monte carlo runs: 10
+Avg precision:  59.2689470154
+Avg recall:  47.9879275654
+
+"""
